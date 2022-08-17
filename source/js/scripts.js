347ef6d020e0b606dd1af89d60a8db66b7583b0f
PolinaT <timoninapa@gmail.com>
2022-08-17 15:45:34 +0300

    сокращение кода для кнопки меню

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
   let noJS = document.querySelector('.main-nav');
   let main_menu = document.querySelector('.main-nav__wrapper');
   let main_button = document.querySelector('.main-nav__toggle');
-  let main_logo = document.querySelector('.header__logo');
 
   noJS.classList.remove('main-nav--no-js');
-  main_logo.classList.remove('header__logo--no-js');
 
   main_button.onclick = function () {
     main_menu.classList.toggle('main-nav__wrapper--open');
