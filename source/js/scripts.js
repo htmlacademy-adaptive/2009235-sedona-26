5e49ed27dfb1067e5ad3d4efebc7b869099553ec
PolinaT <timoninapa@gmail.com>
2022-08-17 16:38:00 +0300

    исправление скрипта

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,12 +1,10 @@
-$(document).ready(function() {
-  let noJS = document.querySelector('.main-nav');
-  let main_menu = document.querySelector('.main-nav__wrapper');
-  let main_button = document.querySelector('.main-nav__toggle');
+let noJS = document.querySelector('.main-nav');
+let main_menu = document.querySelector('.main-nav__wrapper');
+let main_button = document.querySelector('.main-nav__toggle');
 
-  noJS.classList.remove('main-nav--no-js');
+noJS.classList.remove('main-nav--no-js');
 
-  main_button.onclick = function () {
-    main_menu.classList.toggle('main-nav__wrapper--open');
-    main_button.classList.toggle('main-nav__toggle--close');
-  };
-});
+main_button.onclick = function () {
+  main_menu.classList.toggle('main-nav__wrapper--open');
+  main_button.classList.toggle('main-nav__toggle--close');
+};
