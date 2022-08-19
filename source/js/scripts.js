b8237840bb25b4fb75d3c8962074303cf3ed46f8
PolinaT <timoninapa@gmail.com>
2022-08-19 13:42:09 +0300

    поменяла let на const

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,6 @@
-let noJS = document.querySelector('.main-nav');
-let main_menu = document.querySelector('.main-nav__wrapper');
-let main_button = document.querySelector('.main-nav__toggle');
+const noJS = document.querySelector('.main-nav');
+const main_menu = document.querySelector('.main-nav__wrapper');
+const main_button = document.querySelector('.main-nav__toggle');
 
 noJS.classList.remove('main-nav--no-js');
 
